73a18b74ec0d8c1a7531bbbc26bb6c2038806d04
ikhlasdansantai <initesting5@gamil.com>
2023-05-22 15:07:03 +0700

    Menghapus file yang tidak digunakan + memperbaiki penamaan object beserta key dan valuenya

--- src/components/varietyShow/VarietyShow.jsx
+++ src/components/varietyShow/VarietyShow.jsx
@@ -1,68 +1,47 @@
 import Title from "../TitleComp/Title";
 
-import wePlayAlbum from "../../assets/we-play.jpg";
-import playGameAlbum from "../../assets/Weeekly_-_Play_Game_-_Awake.png";
-// import unknownAlbum from "../../assets/unknown.jpg";
-// import AlbumCard from "../albumCard/AlbumCard";
-import wecanAlbum from "../../assets/Weeekly_wecan_2020.jpg";
-
 import weClear from "../../assets/weclear.jpg";
-import unknownShow from "../../assets/unknownShow.jpg";
+import weClearEp2 from "../../assets/weclear-ep2.jpg";
 import pitPatShow from "../../assets/pit-a-pat.jpg";
 
 const VarietyShow = () => {
-  const albums = [
+  const varietyShow = [
     {
-      name: "Play Game Awake",
-      date: "01 JAN 2017",
-      src: playGameAlbum,
+      name: "Pit A Pat",
+      src: pitPatShow,
+      link: "https://www.youtube.com/watch?v=DPreRRBoRbc",
     },
     {
-      name: "We Play",
-      date: "01 JAN 2019",
-      src: wePlayAlbum,
+      name: "weClear",
+      src: weClear,
+      link: "https://www.youtube.com/watch?v=dC4OVecjNmo",
     },
     {
-      name: "We Play",
-      date: "01 JAN 2019",
-      src: wecanAlbum,
-    },
-    {
-      name: "We Can",
-      date: "01 JAN 2019",
-      src: wecanAlbum,
+      name: "We Clear Ep.2",
+      src: weClearEp2,
+      link: "https://www.youtube.com/watch?v=1EX0zQTK93o",
     },
   ];
 
   return (
-    <section id="variety-show" className="container pt-20 px-[1.5rem]">
+    <section id="Shows" className="container pt-20 px-[1.5rem]">
       <div className="container mx-auto">
         <Title title={"VARIETY SHOW"} />
         <div className="video-cards grid lg:grid-cols-3 gap-12 pt-10">
-          <div className="video-card shadow-md group cursor-pointer lg:hover:-mt-2 hover:shadow-[#fff] duration-300 transition-all">
-            <figure className="flex">
-              <img src={weClear} alt="" className="block max-w-full" />
-            </figure>
-            <div className="card-footer p-standart">
-              <h2 className="text-xl text-white leading-relaxed tracking-[.5px]">WeClear</h2>
-            </div>
-          </div>
-          <div className="video-card shadow-md group cursor-pointer lg:hover:-mt-4 hover:shadow-[#fff] duration-300 transition-all">
-            <figure className="flex">
-              <img src={unknownShow} alt="" className="block max-w-full" />
-            </figure>
-            <div className="card-footer p-standart">
-              <h2 className="text-xl text-white leading-relaxed tracking-[.5px]">WeClear</h2>
-            </div>
-          </div>
-          <div className="video-card shadow-md group cursor-pointer lg:hover:-mt-4 hover:shadow-[#fff] duration-300 transition-all">
-            <figure className="flex">
-              <img src={pitPatShow} alt="" className="block max-w-full" />
-            </figure>
-            <div className="card-footer p-standart">
-              <h2 className="text-xl text-white leading-relaxed tracking-[.5px]">WeClear</h2>
-            </div>
-          </div>
+          {varietyShow.map((show, index) => {
+            return (
+              <a key={index} href={show.link}>
+                <div className="video-card shadow-md group cursor-pointer lg:hover:-mt-2 hover:shadow-[#fff] duration-300 transition-all">
+                  <figure className="flex">
+                    <img src={show.src} alt={`${show.src} pict`} className="block max-w-full" />
+                  </figure>
+                  <div className="card-footer p-standart">
+                    <h2 className="text-xl text-white leading-relaxed tracking-[.5px]">{show.name}</h2>
+                  </div>
+                </div>
+              </a>
+            );
+          })}
         </div>
       </div>
     </section>
