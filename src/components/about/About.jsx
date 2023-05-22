55465f306d3724c61dcae6ae406d53e2a5dc4d63
ikhlasdansantai <initesting5@gamil.com>
2023-05-22 15:04:53 +0700

    mengganti bahasa menjadi bahasa inggris

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import Title from "../TitleComp/Title";
 
 const About = () => {
   return (
-    <div id="main" className="container pt-10 px-[1.5rem]">
+    <div id="About" className="container pt-10 px-[1.5rem]">
       <div className="container lg:w-[90rem] mx-auto mt-2">
         <Title title={"ABOUT US"} />
         <div className="about-contents flex flex-col lg:flex-row items-center gap-12 mt-10 lg:mt-20">
@@ -11,8 +11,8 @@ const About = () => {
             <img src={weeeklyPict} alt="" className="mx-auto w-full block" />
           </figure>
           <p className="text-textLight text-justify lg:w-2/4 text-base lg:text-lg lg:leading-[2] leading-loose">
-            WEEEKLY (위클리) (sebelumnya dikenal sebagai PlayM Girls dan FAVE Girls) adalah sebuah grup musik perempuan Korea Selatan yang terdiri dari enam anggota di bawah IST Entertainment (sebelumnya Play M Entertainment). Anggotanya
-            meliputi Lee Soojin, Monday, Park Soeun, Lee Jaehee, Jihan, dan Zoa. Mereka debut pada tanggal 30 Juni 2020 dengan album debut mereka yang berjudul "We Are", yang menampilkan lagu utama "Tag Me (@Me)".
+            WEEEKLY (위클리) (previously known as PlayM Girls and FAVE Girls) is a South Korean girl group consisting of six members under IST Entertainment (previously Play M Entertainment). The members include Lee Soojin, Monday, Park
+            Soeun, Lee Jaehee, Jihan, and Zoa. They made their debut on June 30, 2020, with their debut album titled "We Are," which features the lead single "Tag Me (@Me)".
           </p>
         </div>
       </div>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -13,10 +13,11 @@ import Navbar from "../../components/navbar/Navbar";
 
 const Header = () => {
   const [selectedMember, setSelectedMember] = useState({
-    name: "Han Jih Yo",
-    username: "Jihan",
     src: Jihan,
-    biodata: "Han Ji-hyo atau lebih dikenal sebagai Jihan, adalah seorang penyanyi, dan penari asal Korea Selatan. Ia merupakan anggota dari grup vokal wanita, Weeekly yang berasal dari perusahaan IST Entertainment.",
+    name: "Han Jih Yo",
+    role: "Lead Vocalist",
+    username: "Jihan",
+    biodata: "Han Ji-hyo, or better known as Jihan, is a singer and dancer from South Korea. She is a member of the female vocal group, Weeekly, which is under the IST Entertainment company.",
   });
 
   const handleMemberSelect = (member) => {
@@ -29,56 +30,64 @@ const Header = () => {
     {
       src: soojin,
       name: "Lee Soo Jin",
+      role: "Leader",
       username: "Soojin",
-      biodata: "Lee Soo-jin atau lebih dikenal sebagai Soojin, adalah seorang penyanyi, dan penari asal Korea Selatan. Ia merupakan anggota dari grup vokal wanita, Weeekly yang berasal dari perusahaan IST Entertainment",
+      biodata: "Lee Soo-jin, or better known as Soojin, is a singer and dancer from South Korea. She is a member of the female vocal group, Weeekly, which is under the IST Entertainment company.",
     },
     {
       src: monday,
       name: "Monday",
+      role: "Vocal Leader",
       username: "Monday",
-      biodata: "Monday atau lebih dikenal sebagai Monday, adalah seorang penyanyi, dan penari asal Korea Selatan. Ia merupakan anggota dari grup vokal wanita, Weeekly yang berasal dari perusahaan IST Entertainment",
+      biodata: "Monday, or better known as Monday, is a singer and dancer from South Korea. She is a member of the female vocal group, Weeekly, which is under the IST Entertainment company.",
     },
     {
       src: soeun,
       name: "Park Soeun",
+      role: "Dance Leader",
       username: "soeun",
-      biodata: "soeun atau lebih dikenal sebagai soeun, adalah seorang penyanyi, dan penari asal Korea Selatan. Ia merupakan anggota dari grup vokal wanita, Weeekly yang berasal dari perusahaan IST Entertainment",
+      biodata: "Soeun, or better known as Soeun, is a singer and dancer from South Korea. She is a member of the female vocal group, Weeekly, which is under the IST Entertainment company.",
     },
     {
       src: jaehee,
       name: "Lee Jae Hee",
+      role: "Sub Vocalist",
       username: "Jaehee",
-      biodata: "Lee Jae-hee atau lebih dikenal sebagai Jaehee, adalah seorang penyanyi, dan penari asal Korea Selatan. Ia merupakan anggota dari grup vokal wanita, Weeekly yang berasal dari perusahaan IST Entertainment",
+      biodata: "Lee Jae-hee, or better known as Jaehee, is a singer and dancer from South Korea. She is a member of the female vocal group, Weeekly, which is under the IST Entertainment company.",
     },
     {
       src: Jihan,
       name: "Han Jih Yo",
+      role: "Visual",
       username: "Jihan",
-      biodata: "Han Ji-hyo atau lebih dikenal sebagai Jihan, adalah seorang penyanyi, dan penari asal Korea Selatan. Ia merupakan anggota dari grup vokal wanita, Weeekly yang berasal dari perusahaan IST Entertainment.",
+      biodata: "Han Ji-hyo, or better known as Jihan, is a singer and dancer from South Korea. She is a member of the female vocal group, Weeekly, which is under the IST Entertainment company.",
     },
     {
       src: zoa,
       name: "Jo Hye Won",
+      role: "Maknae",
       username: "Zoa",
-      biodata: "Zoa merupakan member termuda dalam Weeekly dan sering dipanggil dengan sebutan Zoababy oleh para member. Ia pernah tampil dalam VCR Stray Kids di MMA 2018 .",
+      biodata: "Zoa is the youngest member in Weeekly and is often called Zoababy by the other members. She has appeared in Stray Kids' VCR at the 2018 MMA (Melon Music Awards).",
     },
   ];
 
   return (
-    <header id="header">
+    <header id="Home">
       <Navbar />
       <div id="temp" className="container flex max-w-full mx-auto overflow-hidden">
-        <div className="header-bg h-full mx-auto -z-30">
+        <div className="header-bg h-full mx-auto">
           <div className="header-contents container flex flex-col justify-center items-center min-[390px]:pt-[0rem] sm:flex-row sm:py-32 lg:w-[90rem] min-[1700px]:items-start min-[1700px]:py-[5rem]">
             <div className="container p-standart header-content-titles md:w-[70%] min-[1700px]:mt-[10rem]">
-              <small className="tracking-wider text-white font-semibold">Weeekly's {selectedMember.username}</small>
+              <small className="tracking-wider text-white font-semibold">
+                Weeekly's {selectedMember.username} / {selectedMember.role}
+              </small>
               <h1 className="text-5xl md:text-[55px] lg:text-[96px] md:leading-[4rem] lg:leading-[7rem] text-white">{selectedMember.name}</h1>
               <p className="text-slate-100 text-sm lg:text-base  mt-4 lg:w-[60ch] leading-7">{selectedMember.biodata}.</p>
-              <button type="submit" className="border bg-white py-2 px-4 mt-8">
+              {/* <button type="submit" className="border bg-white py-2 px-4 mt-8">
                 Play Video
-              </button>
+              </button> */}
             </div>
-            <div className="header-content-img mt-auto sm:mt-0 flex w-[18rem] min-[350px]:w-[20rem] relative md1111111111:w-[30%] lg:w-[40%]">
+            <div className="header-content-img mt-auto sm:mt-0 flex w-[18rem] min-[350px]:w-[20rem] relative md:w-[30%] lg:w-[40%]">
               <img src={selectedMember.src} alt="Jihan Img" className="block w-full object-fit mx-auto" />
             </div>
           </div>
