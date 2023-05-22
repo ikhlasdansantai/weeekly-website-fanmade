4c9e991315b1cdb24f1ce6164c06c54bd668d86e
ikhlasdansantai <initesting5@gamil.com>
2023-05-22 15:08:14 +0700

    memperbaiki bug tampilan pada foto bg di header

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -1,8 +1,6 @@
-import MoreInfo from "../../components/MoreInfo/MoreInfo";
 import About from "../../components/about/About";
 import Discography from "../../components/discography/discography";
 import Schedule from "../../components/schedule/Schedule";
-// import Member from "../../components/member/Member";
 import VarietyShow from "../../components/varietyShow/VarietyShow";
 import Vote from "../../components/vote/Vote";
 
@@ -14,7 +12,6 @@ const Main = () => {
       <VarietyShow />
       <Schedule />
       <Vote />
-      {/* <MoreInfo /> */}
     </section>
   );
 };
