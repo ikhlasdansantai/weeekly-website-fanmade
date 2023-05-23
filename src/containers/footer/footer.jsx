5a191d8674cffcd45453898db3c6bd64bd94acfd
ikhlasdansantai <initesting5@gamil.com>
2023-05-23 13:52:04 +0700

    menambahkan arialabel pada icon di comp footer

--- src/containers/footer/footer.jsx
+++ src/containers/footer/footer.jsx
@@ -6,26 +6,31 @@ const Footer = () => {
       link: "https://www.instagram.com/_Weeekly/",
       iconClass: "icon-park-outline:instagram",
       hoverClass: "hover:text-red-500",
+      label: "Follow us on Instagram",
     },
     {
       link: "https://www.youtube.com/c/Weeekly",
       iconClass: "teenyicons:youtube-solid",
       hoverClass: "hover:text-[#FF0000]",
+      label: "Follow us on Youtube",
     },
     {
       link: "https://twitter.com/_Weeekly",
       iconClass: "mingcute:twitter-fill",
       hoverClass: "hover:text-[#1DA1F2]",
+      label: "Follow us on Twitter",
     },
     {
       link: "https://www.facebook.com/WeeeklyOfficial",
       iconClass: "uil:facebook",
       hoverClass: "hover:text-[#1877F2]",
+      label: "Follow us on Facebook",
     },
     {
       link: "https://www.tiktok.com/@weeekly",
       iconClass: "ic:round-tiktok",
       hoverClass: "hover:text-[#FF0000]",
+      label: "Follow us on Tiktok",
     },
   ];
 
@@ -36,7 +41,7 @@ const Footer = () => {
           {links.map((link, index) => {
             return (
               <a key={index} href={link.link}>
-                <Icon icon={link.iconClass} className={`text-black text-[1.4rem] ${link.hoverClass}`} />
+                <Icon icon={link.iconClass} aria-label={link.label} className={`text-black text-[1.4rem] ${link.hoverClass}`} />
               </a>
             );
           })}
@@ -47,8 +52,10 @@ const Footer = () => {
         This website is created using ☕🤔💖🤠
         <p className="text-white">
           © 2023{" "}
-          <span className="font-semibold hover:text-textDark">
-            <a href="https://github.com/ikhlasdansantai">ikhlasdansantai</a>
+          <span className="font-semibold hover:text-[#aaa]">
+            <a href="https://github.com/ikhlasdansantai" aria-label="Follow Me On Github">
+              ikhlasdansantai
+            </a>
           </span>
           . All Rights Reserved.
         </p>
