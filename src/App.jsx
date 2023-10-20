3c20d3d3e4106bed62cd090c99b90570e341d004
ikhlasdansantai <initesting5@gmail.com>
2023-10-20 21:33:25 +0700

    masih bingung apakah file App.jsx akan digunakan atau dihapus nantinya

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import "./index.css";
-import Header from "./containers/header/Header";
-import Main from "./containers/main/main";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./containers/footer/footer";
+// import "./index.css";
+// import Header from "./containers/header/Header";
+// import Main from "./containers/main/main";
+// import Navbar from "./components/navbar/Navbar";
+// import Footer from "./containers/footer/footer";
 
-function App() {
-  return (
-    <div className="App overflow-hidden">
-      <Header>
-        <Navbar />
-      </Header>
-      <Main />
-      <Footer />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App overflow-hidden">
+//       <Header>
+//         <Navbar />
+//       </Header>
+//       <Main />
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
