2bb5919fa6f265c95a903c2f41adef25c68a8cfc
ikhlasdansantai <initesting5@gmail.com>
2023-10-20 21:31:49 +0700

    mengimprove kualitas kode beserta penambahan beberapa file baru

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root/Root";
+import NotFound from "./pages/NotFound/NotFound";
+import Home from "./pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../containers/header/Header";
+import Main from "../../containers/main/main";
+
+export default function Home() {
+  return (
+    <div className="App overflow-hidden">
+      <Header />
+      <Main />
+    </div>
+  );
+}
